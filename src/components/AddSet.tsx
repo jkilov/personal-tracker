66d046aeeaec3015ad0c42d56e637977e763624c
josh <josh.kilov@gmail.com>
2026-02-26 14:04:07 +1100

    added ability to dynamically render values for each input per set - still need to add ability to update these with state setter. Still not connect to backend. next step - add onchange capabilities

--- src/components/AddSet.tsx
+++ src/components/AddSet.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 type NewSet = {
-  id: string;
+  id?: string;
   title?: string;
   repLabel: string;
   repInputType: string;
@@ -9,8 +9,13 @@ type NewSet = {
   weightInputType: string;
 };
 
+type SetValuesConfig = {
+  el: NewSet;
+  repVal: number;
+  weightVal: number;
+};
+
 const newSetTemplate: NewSet = {
-  id: crypto.randomUUID(),
   repLabel: "Reps",
   repInputType: "number",
   weightLabel: "Weight",
@@ -30,13 +35,38 @@ const AddSet = () => {
     },
   ]);
 
+  const stateSetValues = createSet.reduce<any>((acc, el) => {
+    acc[el.title] = { ...el, repVal: 0, weightVal: 0 };
+    return acc;
+  }, {});
+
+  const [repData, setRepData] = useState(stateSetValues);
+
   const addSet = () => {
     const newCount = 1 + count;
     setCount(newCount);
 
-    const newSet = { title: `Set ${newCount}`, ...newSetTemplate };
+    const newSet = {
+      title: `Set ${newCount}`,
+      id: crypto.randomUUID(),
+      ...newSetTemplate,
+    };
     setCreateSet((prev) => [...prev, newSet]);
   };
+
+  const handleValChange = (
+    title: string,
+
+    input: string,
+    value: string | number
+  ) => {
+    setRepData((prev) => ({
+      ...prev,
+      [title]: { ...[title], [input]: value },
+    }));
+  };
+
+  console.log("D", repData);
 
   return (
     <div>
@@ -44,9 +74,15 @@ const AddSet = () => {
         <div key={`${set.id}-${count}`}>
           <h4>{set.title}</h4>
           <label htmlFor={set.repLabel}>{set.repLabel}</label>
-          <input type={set.repInputType} />
+          <input
+            type={set.repInputType}
+            value={stateSetValues[set.title!].repVal}
+          />
           <label htmlFor={set.weightLabel}>{set.weightLabel}</label>
-          <input type={set.weightInputType} />
+          <input
+            type={set.weightInputType}
+            value={stateSetValues[set.title!].weightVal}
+          />
         </div>
       ))}
       <span>kg</span>
