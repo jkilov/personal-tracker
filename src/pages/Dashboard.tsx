29626008864c616b8d39b8079cf9327b9cc5bc50
josh <josh.kilov@gmail.com>
2026-03-02 16:02:31 +1100

    added early exit if session has been created for the day already

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,46 +8,47 @@ import { useNavigate } from "react-router";
 const Dashboard = () => {
   const navigate = useNavigate();
   const [userId, setUserId] = useState("");
+  const [isSessionExists, setIsSessionExists] = useState<boolean | undefined>(
+    false
+  );
 
   //does the above need to be in a useEffect as we are making server calls - lso needs loading state
 
   const handleCreateSession = async () => {
+    const data = await fetchSessionData("created_at");
+    const sessionCreatedDate = data.data;
+    const newArr = sessionCreatedDate?.map((date) =>
+      date.created_at.split("T")
+    );
+    const justDate = newArr?.map((date) => date[0]);
+    const todayUTC = new Date().toISOString().split("T")[0];
+    const sessionExists = justDate?.includes(todayUTC);
+    setIsSessionExists(sessionExists);
+
+    if (sessionExists) {
+      console.log("session already exists");
+      return;
+    }
+
     const { data: userData } = await readUserData();
 
     const uid = userData.user_id;
     setUserId(uid);
 
-    const { data } = await createSession(uid);
+    const { data: sessionData } = await createSession(uid);
 
-    const sessionId = data?.session_id;
+    const sessionId = sessionData?.session_id;
 
     navigate(`/modal/${sessionId}`);
   };
 
-  const testSessionDate = async () => {
-    const data = await fetchSessionData("created_at");
-
-    console.log(data.data);
-
-    const testArr = data.data;
-
-    const newArr = testArr?.map((date) => date.created_at.split("T"));
-
-    const justDate = newArr?.map((date) => date[0]);
-
-    const today = new Date().toISOString().split("T")[0];
-    const sessionExists = justDate?.includes(today);
-
-    console.group(today);
-    console.log(sessionExists);
-  };
+  console.log("is", isSessionExists);
 
   return (
     <div>
       <h1>Dashboard</h1>
 
       <button onClick={handleCreateSession}>Add Session</button>
-      <button onClick={testSessionDate}>get Date Test</button>
     </div>
   );
 };
