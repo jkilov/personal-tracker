e44a61beb881c66ac449096e83cf957797c7e9e7
josh <josh.kilov@gmail.com>
2026-03-06 08:25:12 +1100

    removed ui session constraints and tranferred session_Date contraints to backend where SOT lives

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,50 +1,31 @@
 import { useState } from "react";
 import { Toaster, toast } from "sonner";
 import { readUserData } from "../utils/supabase/user";
-import { createSession, fetchSessionData } from "../utils/supabase/session";
+import { createSession } from "../utils/supabase/session";
 import { useNavigate } from "react-router";
 import { PulseLoader } from "react-spinners";
 import "../App.css";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  // const [userId, setUserId] = useState("");
-  // const [isSessionExists, setIsSessionExists] = useState<boolean | undefined>(
-  //   false
-  // );
 
   const [isLoading, setIsLoading] = useState(false);
 
-  //does the above need to be in a useEffect as we are making server calls - lso needs loading state
-
-  //TODO: the below needs to be broken jp into helper functions
-  //TODO: need to add some loading indicators when API calls are being made
   const handleCreateSession = async () => {
     setIsLoading(true);
 
-    const data = await fetchSessionData("created_at");
-    const sessionCreatedDate = data.data;
-    const newArr = sessionCreatedDate?.map((date) =>
-      date.created_at.split("T")
-    );
-    const justDate = newArr?.map((date) => date[0]);
-    const todayUTC = new Date().toISOString().split("T")[0];
-    const sessionExists = justDate?.includes(todayUTC);
-    // setIsSessionExists(sessionExists);
+    const { data: userData } = await readUserData();
+    const uid = userData.user_id;
 
-    if (sessionExists) {
+    const { data: sessionData, error } = await createSession(uid);
+
+    if (error) {
       toast.error("session already exists", {
         style: { background: "var(--error)" },
       });
       setIsLoading(false);
       return;
     }
-
-    const { data: userData } = await readUserData();
-    const uid = userData.user_id;
-    // setUserId(uid);
-
-    const { data: sessionData } = await createSession(uid);
 
     const sessionId = sessionData?.session_id;
     setIsLoading(true);
