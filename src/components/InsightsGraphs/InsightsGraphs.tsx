60865e6c54f18f2fd4164e7b3fde52d337ae20d5
josh <josh.kilov@gmail.com>
2026-04-30 08:05:19 +1000

    line graph complete

--- src/components/InsightsGraphs/InsightsGraphs.tsx
+++ src/components/InsightsGraphs/InsightsGraphs.tsx
@@ -1,100 +1,30 @@
 import { OrbitProgress } from "react-loading-indicators";
-import {
-  type FetchedFitnessScores,
-  useGraphMetrics,
-} from "../../components/InsightsGraphs/useGraphMetrics";
-import { useState, useEffect } from "react";
+import { useGraphMetrics } from "../../components/InsightsGraphs/useGraphMetrics";
 
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
+import { RadarChart, PolarGrid } from "recharts";
+import LineGraph from "./LineGraph";
 // import { RechartsDevtools } from '@recharts/devtools';
 
-type SessionDate = {
-  session_date: string;
-};
-
-type FormattedFitnessData = {
-  Date: string;
-  total_daily_volume: number;
-  adjusted_daily_volume: number;
-  session: SessionDate[];
-};
-
 const InsightsGraphs = () => {
-  const { data, error, isLoading } = useGraphMetrics();
-  const [formattedFitnessData, setFormattedFitnessData] = useState<
-    FormattedFitnessData[]
-  >([]);
-  useEffect(() => {
-    const formattedData = data.map((el) => ({
-      Date: el.session.session_date,
-      total_daily_volume: el.total_daily_volume,
-      adjusted_daily_volume: el.adjusted_daily_volume,
-    }));
-
-    formattedData.sort(
-      (a, b) => new Date(a.Date).getTime() - new Date(b.Date).getTime()
-    );
-
-    setFormattedFitnessData(formattedData);
-  }, [data]);
+  const { data: rawTrainingData, error, isLoading } = useGraphMetrics();
 
   if (isLoading) {
     return <OrbitProgress color="#7CEA9C" size="medium" text="" textColor="" />;
   }
 
-  console.log("data", data);
-  console.log("t", formattedFitnessData);
+  console.log("R", rawTrainingData);
 
   return (
     <div style={{ width: "100vw" }}>
-      <LineChart
-        style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+      <LineGraph rawTrainingData={rawTrainingData} />
+      {/* <RadarChart
+        style={{ width: "100%", aspectRatio: 1.618, maxWidth: "600px" }}
+        outerRadius="80%"
         responsive
         data={formattedFitnessData}
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 5,
-          left: 0,
-        }}
       >
-        <CartesianGrid stroke="var(--font)" strokeDasharray="5" />
-        <Line
-          dataKey="adjusted_daily_volume"
-          type="monotone"
-          strokeWidth={3}
-          name="Adjusted Daily Volume"
-          stroke="#7494EA"
-          dot={{ fill: "var(--background)" }}
-        />
-        <Line
-          dataKey="total_daily_volume"
-          type="monotone"
-          strokeWidth={3}
-          name="Total Daily Volume"
-          stroke="#7CEA9C"
-          dot={{ fill: "var(--background)" }}
-        />
-        <XAxis dataKey="Date" />
-        <YAxis
-          width="auto"
-          label={{ value: "KG", position: "insideLeft", angle: -90 }}
-        />
-        <Legend align="center" />
-        <Tooltip
-          contentStyle={{
-            backgroundColor: "var(--background)",
-          }}
-        />
-      </LineChart>
+        <PolarGrid />
+      </RadarChart> */}
     </div>
   );
 };
---
+++ src/components/InsightsGraphs/LineGraph.tsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+interface Props {
+  rawTrainingData: any[];
+}
+
+type SessionDate = {
+  session_date: string;
+};
+
+type FormattedFitnessData = {
+  Date: string;
+  total_daily_volume: number;
+  adjusted_daily_volume: number;
+  session: SessionDate[];
+};
+
+const LineGraph = ({ rawTrainingData }: Props) => {
+  const [formattedFitnessData, setFormattedFitnessData] = useState<
+    FormattedFitnessData[]
+  >([]);
+
+  useEffect(() => {
+    const formattedData = rawTrainingData.map((el) => ({
+      Date: el.session.session_date,
+      total_daily_volume: el.total_daily_volume,
+      adjusted_daily_volume: el.adjusted_daily_volume,
+    }));
+
+    formattedData.sort(
+      (a, b) => new Date(a.Date).getTime() - new Date(b.Date).getTime()
+    );
+
+    setFormattedFitnessData(formattedData);
+  }, [rawTrainingData]);
+  return (
+    <div>
+      <LineChart
+        style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+        responsive
+        data={formattedFitnessData}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 5,
+          left: 0,
+        }}
+      >
+        <CartesianGrid stroke="var(--font)" strokeDasharray="5" />
+        <Line
+          dataKey="adjusted_daily_volume"
+          type="monotone"
+          strokeWidth={3}
+          name="Adjusted Daily Volume"
+          stroke="#7494EA"
+          dot={{ fill: "var(--background)" }}
+        />
+        <Line
+          dataKey="total_daily_volume"
+          type="monotone"
+          strokeWidth={3}
+          name="Total Daily Volume"
+          stroke="#7CEA9C"
+          dot={{ fill: "var(--background)" }}
+        />
+        <XAxis dataKey="Date" />
+        <YAxis
+          width="auto"
+          label={{ value: "KG", position: "insideLeft", angle: -90 }}
+        />
+        <Legend align="center" />
+        <Tooltip
+          contentStyle={{
+            backgroundColor: "var(--background)",
+          }}
+        />
+      </LineChart>
+    </div>
+  );
+};
+
+export default LineGraph;
